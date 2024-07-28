f3bbd00cbd200579bffc6131dc7f207942be0f11
Supremacy <musaabdulmuqaddas@gmail.com>
2024-07-28 10:59:44 -0500

    Fixed mobile nav fixed position issue

--- components/profile/profileLayouts/profileLayouts.tsx
+++ components/profile/profileLayouts/profileLayouts.tsx
@@ -5,7 +5,7 @@ import ProfileContentMenu from "./ProfileContentMenu";
 
 const ProfileLayout: React.FC = () => {
   return (
-      <main className="font-nunito mt-7 pb-24 lg:mt-28 mx-auto max-w-[993px] xl:max-w-[1200px] xxl:px-0 xl:mt-32 lg:flex xl:gap-2 xxl:gap-10">
+      <main className="font-nunito pt-24 pb-24 lg:mt-28 mx-auto max-w-[993px] xl:max-w-[1200px] xxl:px-0 xl:mt-32 lg:flex xl:gap-2 xxl:gap-10">
         <div className="lg:w-[40%] xl:w-[35%] xxl:w-[32%]">
           <ProfileContentMenu />
         </div>
